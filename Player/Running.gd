cdb6b8d7555572af01aacb52d8dead1c74b843aa
Alexzander Stone <alexzas@g.clemson.edu>
2018-10-10 15:18:01 -0400

    Alternating movement inputs no longer cancel running.

--- Player/Running.gd
+++ Player/Running.gd
@@ -1,16 +1,23 @@
 extends "res://State Machine/State.gd"
 
+var currentInputs = [0, 0, 0, 0]
+
 func update(delta):
-	# If player wishes to move left, the value will be -1. If they move right, it's 1.
-	# And if they press both, it's 0.
-	var horizontalMove = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	var verticalMove = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	if(horizontalMove == 0 and verticalMove == 0):
+	updateRapidMovementArray(currentInputs)
+	
+	# Check to see if no input has been found, transition to idling if true.
+	var moveCounter = 0
+	for input in currentInputs:
+		if(input != 0):
+			moveCounter += 1
+	if(moveCounter == 0):
 		emit_signal("finished", "idling")
 	
 	# Need to account for diagonal movement. Instead of the movement being represented as a square, it is a circle.
 	# Then find the coordinates on the circle based on the angle of the 
 	# original horizontal and vertical movement.
+	var horizontalMove = currentInputs[1] - currentInputs[0]
+	var verticalMove = currentInputs[3] - currentInputs[2]
 	if(horizontalMove != 0 and verticalMove != 0):
 		var playerAngle = atan(abs(verticalMove) / abs(horizontalMove))
 		owner.velocity.x = cos(playerAngle) * horizontalMove * owner.RUN_VELOCITY
@@ -20,3 +27,9 @@ func update(delta):
 		owner.velocity.y = verticalMove * owner.RUN_VELOCITY 
 	
 	owner.update_position()
+
+func updateRapidMovementArray(array):
+	array[0] = int(Input.is_action_pressed("ui_left"))
+	array[1] = int(Input.is_action_pressed("ui_right"))
+	array[2] = int(Input.is_action_pressed("ui_up"))
+	array[3] = int(Input.is_action_pressed("ui_down")) 
