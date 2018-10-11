56b05ee94039bb454555748ad690cfef816ab4f3
Alexzander Stone <alexzas@g.clemson.edu>
2018-10-11 16:21:07 -0400

    Initial state name is represented correctly

--- Player/Label.gd
+++ Player/Label.gd
@@ -7,6 +7,7 @@ var state_machine
 func _ready():
 	state_machine = owner.find_node("StateMachine")
 	state_machine.connect("state_changed", self, "change_state_name")
+	text = str(state_machine.current_state.name)
 
 func change_state_name(current_state):
 	text = str(current_state.name)
