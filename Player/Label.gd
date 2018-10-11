510012267d241472b4ea91a512f7681ebec28710
Alexzander Stone <alexzas@g.clemson.edu>
2018-10-11 16:17:48 -0400

    Added canvas nodes to dynamically resize UI debug elements.

---
+++ Player/Label.gd
@@ -0,0 +1,12 @@
+extends Label
+
+signal state_changed(current_state)
+
+var state_machine
+
+func _ready():
+	state_machine = owner.find_node("StateMachine")
+	state_machine.connect("state_changed", self, "change_state_name")
+
+func change_state_name(current_state):
+	text = str(current_state.name)
