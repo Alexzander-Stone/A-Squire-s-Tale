af305f04c6375cd783842d43b701109e673537b6
Alexzander Stone <alexzas@g.clemson.edu>
2018-10-12 15:04:35 -0400

    Added a small animation timer to test the casting state until proper animations are added.

--- Player/Casting.gd
+++ Player/Casting.gd
@@ -1,8 +1,18 @@
 extends "res://State Machine/State.gd"
 
+var casting_animation_timer = 0
+var timeToAnimate = 2
+
 func enter():
+	casting_animation_timer = timeToAnimate
+	
 	# Will substitute with search for proper ability/animation.
 	if(owner.crafting_container.size() > 0):
 		for i in owner.crafting_container:
 			print("Cast spell " + str(i))
-	emit_signal("finished", "idling")
+
+func update(delta):
+	if(casting_animation_timer >= 0):
+		casting_animation_timer -= delta
+	else:
+		emit_signal("finished", "idling")
