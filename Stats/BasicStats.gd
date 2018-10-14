1399b987b2e3c0c6ff0ef3491ef162993912921b
wluttre <wluttre@g.clemson.edu>
2018-10-14 15:10:15 -0400

    Separated generic and player specific stats
    separated generic stats and player specific stats into two scripts, so that stats can be assigned to generic characters as well as the player.

---
+++ Stats/BasicStats.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# generic stats for all enemies
+export(int) var  maxHealth = 100
+export(int) var currentHealth = 100
+export(int) var walkSpeed = 5
+export(int) var runSpeed = 10
+export(int) var damage = 10
+export(float) var attackSpeed = 1.0
---
+++ Stats/PlayerStats.gd
@@ -0,0 +1,14 @@
+extends "res://Stats/BasicStats.gd"
+
+
+
+#stats specific to player
+
+#resource bar variable, may not need it for prototype
+export(int) var maxResource = 0
+export(int) var currentResource = 0
+export(float) var cdr = 1.0
+export(int) var experience = 0
+export(int) var level = 1
+export(int) var gold = 0
+
