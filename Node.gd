ae67f10a3eceeeb18889602500c19e5a1fe4dff1
wluttre <wluttre@g.clemson.edu>
2018-10-14 14:32:15 -0400

    Added Stats node
    Added a stats node with a script that initializes variables needed for the player character

---
+++ Node.gd
@@ -0,0 +1,29 @@
+extends Node
+
+
+# just listing all potential stats for the PC
+
+
+#could be used for enemies as well
+var health = 100
+var walkSpeed = 5
+var runSpeed = 10
+var damage = 10
+
+#probably player specific stats 
+var attackSpeed = 1.0
+var cdr = 1.0
+var gold = 0
+#var resourceBar = 0
+var experience = 0
+var level = 1
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
