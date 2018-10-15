db35d5ebc085a4ab0b829ece936e94383c5ea811
wluttre <wluttre@g.clemson.edu>
2018-10-15 11:51:01 -0400

    Re ordered some of the code for clarity

--- Stats/PlayerStats.gd
+++ Stats/PlayerStats.gd
@@ -4,11 +4,13 @@ extends "res://Stats/BasicStats.gd"
 
 #stats specific to player
 
-#resource bar variable, may not need it for prototype
-export(int) var maxResource = 0
-export(int) var currentResource = 0
+
 export(float) var cdr = 1.0
 export(int) var experience = 0
 export(int) var level = 1
 export(int) var gold = 0
+
+#resource bar variables, may not need it for prototype
+export(int) var maxResource = 100
+export(int) var currentResource = 0
 
