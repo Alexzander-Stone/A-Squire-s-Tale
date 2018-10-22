82d5c8d97ac877461c7db65874e3a5c389c3c2bc
wluttre <wluttre@g.clemson.edu>
2018-10-22 16:16:06 -0400

    Implemented Dummy hitbox for testing

--- Player/Scripts/Movement States/Casting.gd
+++ Player/Scripts/Movement States/Casting.gd
@@ -2,6 +2,8 @@ extends "res://Player/Scripts/Movement States/Inheritable Classes/IFlinchable.gd
 
 var casting_animation_timer = 0
 var timeToAnimate = 2
+#current iteration: each ability has a number, enter determines animation, duration, etc. based on this
+var abilityNumber = 0
 
 func enter():
 	casting_animation_timer = timeToAnimate
@@ -10,6 +12,11 @@ func enter():
 	if(owner.crafting_container.size() > 0):
 		for i in owner.crafting_container:
 			print("Cast spell " + str(i))
+	
+	#code for implementing spell 1
+	if(abilityNumber == 1)
+		#select animation and play it
+		#set timeToAnimate to the animation duration
 
 func update(delta):
 	if(casting_animation_timer >= 0):
