de71c8f7d4e3acaf17084587a64b0d2886137abe
TraceyKing864 <TraceyKing1864@gmail.com>
2018-10-28 16:55:20 -0400

    Player uses kinematic body 2D and collisions work somewhat

--- Player/Scripts/Player.gd
+++ Player/Scripts/Player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 var MOVE_VELOCITY = 0
@@ -7,13 +7,10 @@ var crafting_container = []
 
 func _ready():
 	print("PC created")
-	get_node("KinematicBody2D").position = position
 
 func update_position():
 	#position += velocity
-	if get_node("KinematicBody2D").move_and_collide(velocity):
-		print("collide")
-	position = get_node("KinematicBody2D").position
+	move_and_collide(velocity)
 
 func add_to_crafting(integer):
 	crafting_container.append(integer)
