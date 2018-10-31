bb7f25f6c12b6c7c78f6002e66964d20d567f18d
alexzander-stone <alexzas@g.clemson.edu>
2018-10-31 14:35:13 -0400

    Generalized damage value GUI for multi-use in various classes (Player, enemies, etc.).

--- UI/Scripts/Damage Value/Damage Value.gd
+++ UI/Scripts/Damage Value/Damage Value.gd
@@ -1,35 +1,34 @@
 extends Node
 
-var tween_node
-var label_node 
-var dummy_collision_node
-var parent_node
+export(NodePath) var collision_path
+export(NodePath) var animated_sprite_path
+
+onready var collision_node = get_node(collision_path)
+onready var animated_sprite_node = get_node(animated_sprite_path)
+onready var tween_node = $"Tween"
+onready var damage_label_node = $"Damage Label"
 
 var texture_height
 var texture_width
 
 func _ready():
-	parent_node = $"../Full Sprite/AnimatedSprite"
-	dummy_collision_node = $"../Full Sprite/AnimatedSprite/Area2D"
-	dummy_collision_node.connect("area_entered", self, "emit_damage")
-	tween_node = $"Tween"
-	label_node = $"RichTextLabel"
+	collision_node.connect("area_entered", self, "emit_damage")
 	
-	texture_height = parent_node.frames.get_frame(parent_node.animation, 0).get_height()
-	texture_width = parent_node.frames.get_frame(parent_node.animation, 0).get_width()
+	texture_height = animated_sprite_node.frames.get_frame(animated_sprite_node.animation, 0).get_height()
+	texture_width = animated_sprite_node.frames.get_frame(animated_sprite_node.animation, 0).get_width()
 
 func emit_damage(damageValue):
 	damageValue=40
 	# Reset position/modulation.
 	reset_tween_settings()
 	# Update text of visual to represent damage taken.
-	label_node.clear()
-	label_node.add_text(str(damageValue))
+	damage_label_node.clear()
+	damage_label_node.add_text(str(damageValue))
 	var goto = get_position() + Vector2(randi()%81 - 40,randi()%81 - 40)
 	tween_node.interpolate_property(self, 'rect_position', get_position(), goto, 1, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween_node.interpolate_property(self, 'modulate', get_modulate(), Color("00ffffff"), 1.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween_node.start()
 
 func reset_tween_settings():
-	set_position(parent_node.get_position() + Vector2(texture_width/4, -texture_height/2))
+	set_position(animated_sprite_node.get_position() + Vector2(texture_width/4, -texture_height/2))
 	set_modulate(Color("ffffff"))
