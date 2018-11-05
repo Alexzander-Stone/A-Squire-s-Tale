6de37f5b9860973c580785d9d1fc7014e4001474
Alexzander Stone <alexzas@g.clemson.edu>
2018-11-04 21:08:28 -0500

    Abilities, Cooldowns, and Casting are linked together.
    Separated Abilities and Cooldowns into their own nodes.

---
+++ Scripts/Abilities/Abilities.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var current_active_abilities = []
+
+func spawn_primary():
+	pass
+
+func spawn_secondary():
+	pass
+
+func spawn_ternary():
+	pass
+
+func spawn_super():
+	pass
--- Scripts/Player/Movement States/Casting.gd
+++ Scripts/Player/Movement States/Casting.gd
@@ -6,10 +6,12 @@ signal ternary_used()
 signal super_used()
 
 export(NodePath) var cooldown_path
+export(NodePath) var abilities_path
 export(NodePath) var weapon_animation_path
 export(NodePath) var parent_path
 
 onready var cooldown_node = get_node(cooldown_path)
+onready var abilities_node = get_node(abilities_path)
 onready var weapon_animation_node = get_node(weapon_animation_path)
 onready var parent_node = get_node(parent_path)
 
@@ -18,6 +20,13 @@ var dict = {"00" : "Primary_Attack_1R", "10" : "Primary_Attack_1R", "11" : "Prim
 var casting_animation_timer = 0
 # Needs to be replaced by ability lengths eventually.
 var timeToAnimate = 2
+
+func _ready():
+	self.connect("primary_used", abilities_node, "spawn_primary")
+	self.connect("secondary_used", abilities_node, "spawn_secondary")
+	self.connect("ternary_used", abilities_node, "spawn_ternary")
+	self.connect("super_used", abilities_node, "spawn_super")
+
 
 func enter(args):
 	if(args.size() > 0):
@@ -30,12 +39,14 @@ func enter(args):
 		#receive input for primary ability (number 1)
 		#add condition checking for cooldown
 		if(args[0] == 1 && cooldown_node.primary_cooldown_timer <= 0):
+			emit_signal("primary_used")
 			#select animation and play it. Rotate based on player's current direction.
 			weapon_animation_node.play(dict[str(round(parent_node.direction_vector[0])) + str(round(parent_node.direction_vector[1]))])
 			print("first ability activate")
-			emit_signal("primary_used")
 			#set timeToAnimate to the animation duration
 			# Need to replace with inheritable helper method.
+			
+			# Get casting_animation_timer from Abilities node.
 			casting_animation_timer = weapon_animation_node.current_animation_length / weapon_animation_node.playback_speed;
 		else:
 			print("no casting")
