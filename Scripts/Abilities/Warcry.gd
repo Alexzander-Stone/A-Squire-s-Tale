c48342135712630a71b53a9318602c5036e59ead
alexzander-stone <alexzas@g.clemson.edu>
2018-11-20 14:13:46 -0500

    All ability nodes are now using their inheritable weapon animation parent.

--- Scripts/Abilities/Warcry.gd
+++ Scripts/Abilities/Warcry.gd
@@ -1,29 +1,16 @@
-extends Area2D
-
-export(String) var statusEffect = "Warcry"
-
-var damageMod = 0.0
-var default_damage = 0
-export(bool) var follow_player = true
+extends "res://Scripts/Abilities/Inheritables/IAnimatedWeapon.gd"
 
 signal giveStatus
+
+export(String) var statusEffect = "Warcry"
 
 func _ready():
 	#for now, warcry works to give all players a movespeed and damage boost
 	for p in get_tree().get_nodes_in_group("player"):
 		self.connect("giveStatus", p, "addStatusEffect")
-	
-	
 
 func _process(delta):
 	if !$"AnimationPlayer".is_playing():
-		print("Warcry complete")
 		emit_signal("giveStatus", statusEffect)
-		queue_free()
-
-func play_animation(animation_to_play):
-	$"AnimationPlayer".play(animation_to_play)
-
-# Allows the weapon to calculate what damage to return using the parent's stats.
-func calculate_damage():
-	return default_damage*damageMod
+	# Inheritable parent takes care of deletion of node.
+	._process(delta)
