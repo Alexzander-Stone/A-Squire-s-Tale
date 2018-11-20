872ff445aa82fd71c408a3416d6fc68c0201140b
Alexzander Stone <alexzas@g.clemson.edu>
2018-11-19 22:13:42 -0500

    Updated Stopwatch comments and setup.

--- Scripts/Artifacts/BaseArtifact.gd
+++ Scripts/Artifacts/BaseArtifact.gd
@@ -1,33 +1,22 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 signal artifact_acquired
+
 export(String) var artifactName = "NULL"
 
 export(NodePath) var collision_path 
-var collision_node
+onready var collision_node = get_node(collision_path)
 
 func _ready():
-	collision_node = get_node(collision_path)
 	collision_node.connect("area_entered", self, "collisionDetected")
 	
-	##
-	#connecting to the player node, allows for multiple players
-	#player is currently added to the group in the PlayerStats node, not the base node
-	##
+	# Multiple player's means that all of them should be able to pick up on the trinket.
+	# We use the Player group to attach the collision signals, since all playable
+	# characters are found there.
 	var player_group = get_tree().get_nodes_in_group("player")
 	for p in player_group:
 		self.connect("artifact_acquired", p, "addArtifact")
 	
 func collisionDetected(colliding_object):
 	emit_signal("artifact_acquired", [artifactName])
-	#queue for deletion at the end of the frame
 	queue_free()
-	
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
