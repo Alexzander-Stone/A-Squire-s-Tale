b14e8d866439ce4939479c9d694aca5e3e61d33c
Alexzander Stone <alexzas@g.clemson.edu>
2018-11-19 23:44:22 -0500

    Clean up of unnecessary signals and debugging statements.

--- Scripts/Player/Movement States/Casting.gd
+++ Scripts/Player/Movement States/Casting.gd
@@ -6,13 +6,11 @@ signal crafting_used(ability_key, animation_to_play)
 
 export(NodePath) var cooldown_path
 export(NodePath) var abilities_path
-export(NodePath) var weapon_animation_path
-export(NodePath) var parent_path
 export(NodePath) var animated_sprite_path
 
 onready var cooldown_node = get_node(cooldown_path)
 onready var abilities_node = get_node(abilities_path)
-onready var parent_node = get_node(parent_path)
+onready var owner_node = owner
 onready var animated_sprite_node = get_node(animated_sprite_path)
 
 var dict = {"00" : "Attack_1R", 
@@ -32,13 +30,13 @@ var timeToAnimate = 2
 func _ready():
 	self.connect("crafting_used", abilities_node, "craft_ability")
 
-
+# Determine which ability to use, based on the PlayerAbilities.JSON file.
+# Each ability will have a unique key ID, weapon indicator animation, and casting timer.
 func enter(args):
 	.enter(args)
 	
 	# Verify that the player hasn't entered the casting state without abilities being used.
 	if args[0] == -1 and owner.crafting_container.size() <= 0:
-		print("no casting")
 		emit_signal("finished", "idling", [])
 	
 	# No crafting involved, single input and straight to casting.
@@ -54,11 +52,10 @@ func enter(args):
 		if cooldown_node.is_cooldown_running_for(i) == true:
 			return
 		ability_key += String(i)
-	# Weapon sprite/visual indicator animation to play.
-	var weapon_animation_to_play = dict[str(round(parent_node.direction_vector[0])) + str(round(parent_node.direction_vector[1]))]
+
+	var weapon_animation_to_play = dict[str(round(owner_node.direction_vector[0])) + str(round(owner_node.direction_vector[1]))]
 	emit_signal("crafting_used", ability_key, weapon_animation_to_play)
-	# Get casting_animation_timer from Abilities node.
-	# Also, set Character animation to play.
+
 	if abilities_node.craftable_abilities_dict.has(ability_key):
 		casting_animation_timer = float(abilities_node.craftable_abilities_dict[ability_key]["casting_timer"])
 		animated_sprite_node.animation = abilities_node.craftable_abilities_dict[ability_key]["character_animation"]
@@ -67,6 +64,9 @@ func update(delta):
 	if(casting_animation_timer >= 0):
 		casting_animation_timer -= delta
 	else:
+		##
+		# May want to implement pushdown automata to return to previous running state.
+		##
 		emit_signal("finished", "idling", [])
 	
 	# Call parent class function.
